e42bb36b7e37f4a07be85856fa8a7957f8befd15
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2017-12-04 12:17:40 +0100

    Réorganisation du fichier adc.c Amélioration de la sortie des menus

--- Src/adc.c
+++ Src/adc.c
@@ -25,7 +25,7 @@ extern uint8_t ready;
 
 void test_adc(state mode){
 	uint32_t sortir_etat=0;
-	uint16_t value_IN0, value_TEMP, value_CALIB,value_DAC;
+	uint16_t value_IN0, value_TEMP, value_CALIB,value_DAC=0;
 	uint16_t tab_value_DAC[]={0,500,1000,1500,2000,2500,3000,3500,4000,4095};
 	int32_t temperature;
 	uint32_t i;
@@ -44,81 +44,76 @@ void test_adc(state mode){
 			while(ready!=1);	//Press Enter
 			ready=0;
 
-
 			while(sortir_etat==0){
+				/***** Gestion du DAC *****/
 				HAL_DAC_SetValue(&hdac,DAC_CHANNEL_1,DAC_ALIGN_12B_R,value_DAC);
 				HAL_Delay(4);
-				//i=2048;
-				value_DAC=(value_DAC+10)%4096;
+				value_DAC=(value_DAC+20)%4096;
+				/***** Gestion de l'ADC *****/
 				HAL_ADC_Start(&hadc);
 				HAL_ADC_PollForConversion(&hadc,HAL_MAX_DELAY);
 				value_IN0=HAL_ADC_GetValue(&hadc);
+				/***** Calcul et affichage de la valeur convertie *****/
 				tension=value_IN0*3.3/4096;
-				snprintf(msg,TAILLE_BUF,"Valeur ADC : \t%u \t\tTension : \t%.3f Volt                   \r", value_IN0, tension);
+				snprintf(msg,TAILLE_BUF,"Tension : %5.3f Volt \t ADC[%4u] \t\t                  \r", tension, value_IN0);
 				HAL_UART_Transmit(&huart2,msg,strlen(msg),HAL_MAX_DELAY);
-				HAL_Delay(50);
-				if(	(ready==1) && (rx_buffer[0]=='c')	){
-					sortir_etat=1;
-					snprintf(msg,TAILLE_BUF,"\r\n");
-					HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
-					HAL_ADC_Stop(&hadc);
-				}
-				else{
-					ready=0;		// mais on reste dans le mode GPIO_POLLING
+				HAL_Delay(100);
+				/***** Test Sortie du menu *****/
+				if(ready==1){
+					ready=0;
+					if(rx_buffer[0]=='c'){
+						sortir_etat=1;
+					}
 				}
 			}
-			ready=0;
+			/***** Sortie du menu *****/
+			HAL_ADC_Stop(&hadc);
+			snprintf(msg,TAILLE_BUF,"\r\n");
+			HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
 			break;
 
 		case TEST_ADC_TEMPERATURE :
 			snprintf(msg,TAILLE_BUF,"ADC : Lecture IN0 et Lecture Temperature Sensor (Internal)\r\n");
 			HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
-			// Sampling time is set to 160.5 for the temperature sensor (see datasheet)
 
 			while(sortir_etat==0){
-
+				/***** Gestion du DAC *****/
 				HAL_DAC_SetValue(&hdac,DAC_CHANNEL_1,DAC_ALIGN_12B_R,value_DAC);
 				HAL_Delay(4);
 				value_DAC=(value_DAC+500)%4096;
-
+				/***** Gestion de l'ADC *****/
 				HAL_ADC_Start(&hadc);
 				HAL_ADC_PollForConversion(&hadc,1000);
 				value_IN0=HAL_ADC_GetValue(&hadc);
-
-				//HAL_ADC_Start(&hadc);			// A mettre si fonctionnement en Discontinous
 				HAL_ADC_PollForConversion(&hadc,1000);
 				value_TEMP=HAL_ADC_GetValue(&hadc);
-
+				/***** Calcul et Afficage des valeurs converties *****/
 				tension=value_IN0*3.3/4096;
-				//temperature=130-30 /CAL2-CAL1 (DATA - CAL1) + 30°C
 				temperature = ( (130-30) * (value_TEMP - *TS_CAL1_ADDR) / (*TS_CAL2_ADDR - *TS_CAL1_ADDR) ) + 30;
-				snprintf(msg,TAILLE_BUF,"Temperature :\t%5d °C\t[%4u]                   \n\r", temperature, value_TEMP);
+				snprintf(msg,TAILLE_BUF,"Temperature :\t%5d °C\tADC[%4u]                   \n\r", temperature, value_TEMP);
 				HAL_UART_Transmit(&huart2,msg,strlen(msg),HAL_MAX_DELAY);
-				snprintf(msg,TAILLE_BUF,"Tension :\t%5.3f Volt\t[%4u]                   \n\r\r\n", tension,  value_IN0);
+				snprintf(msg,TAILLE_BUF,"Tension :\t%5.3f Volt\tADC[%4u]                   \n\r\r\n", tension,  value_IN0);
 				HAL_UART_Transmit(&huart2,msg,strlen(msg),HAL_MAX_DELAY);
-
 				HAL_Delay(3000);
-				if(	(ready==1) && (rx_buffer[0]=='c')	){
-					sortir_etat=1;
-					snprintf(msg,TAILLE_BUF,"\r\n");
-					HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
-					HAL_ADC_Stop(&hadc);
-				}
-				else{
-					ready=0;		// mais on reste dans le mode GPIO_POLLING
+				/***** Test Sortie du menu *****/
+				if(ready==1){
+					ready=0;
+					if(rx_buffer[0]=='c'){
+						sortir_etat=1;
+					}
 				}
 			}
-			ready=0;
+			/***** Sortie du menu *****/
+			HAL_ADC_Stop(&hadc);
+			snprintf(msg,TAILLE_BUF,"\r\n");
+			HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
 			break;
 
 
 		case TEST_ADC_CALIB :
 			snprintf(msg,TAILLE_BUF,"Conversion AVANT Calibration\r\n");
 			HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
-			//HAL_ADCEx_Calibration_Start(&hadc,ADC_SINGLE_ENDED);
-			//value_CALIB=HAL_ADCEx_Calibration_GetValue(&hadc,ADC_SINGLE_ENDED);
-
-
+			/***** Conversion SANS Calibration *****/
 			for(i=0;i<10;i++){
 						HAL_DAC_SetValue(&hdac,DAC_CHANNEL_1,DAC_ALIGN_12B_R,tab_value_DAC[i]);
 						HAL_Delay(4);
@@ -129,7 +124,7 @@ void test_adc(state mode){
 						HAL_UART_Transmit(&huart2,msg,strlen(msg),HAL_MAX_DELAY);
 						HAL_ADC_Stop(&hadc);
 			}
-
+			/***** Conversion APRES Calibration *****/
 			snprintf(msg,TAILLE_BUF,"\r\nConversion APRES Calibration\r\n");
 			HAL_UART_Transmit(&huart2,msg,strlen(msg),1000);
 			HAL_ADCEx_Calibration_Start(&hadc,ADC_SINGLE_ENDED);
@@ -148,6 +143,7 @@ void test_adc(state mode){
 			}
 
 			break;
+
 	}
 
 }
